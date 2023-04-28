8f4d805c9333bc27ba2fca4930b021c76ec375bd
gfddbsdyreyhdye4r5 <97019395+gfddbsdyreyhdye4r5@users.noreply.github.com>
2023-04-28 11:16:04 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    timer += 1
+    basic.showNumber(timer)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    timer += -1
+    basic.showNumber(timer)
+})
+let timer = 0
+basic.showNumber(timer)
+basic.forever(function () {
+	
+})
